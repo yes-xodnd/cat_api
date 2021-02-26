44426a029519fd0fd10ceb418075b886533f33bb
yes-xodnd <taew3167@gmail.com>
2021-02-26 16:43:48 +0900

    Add component: ComponentTemplate

---
+++ src/ComponentTemplate.js
@@ -0,0 +1,19 @@
+import Component from './Component.js';
+
+export default class ComponentName extends Component {
+
+  template = '';
+
+  constructor(props) {
+    super(props);
+    this.init();
+  }
+
+  created() {
+    
+  }
+
+  beforeRender() {
+    
+  }
+}
