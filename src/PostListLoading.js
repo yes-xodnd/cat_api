f6b2c2f441499a2d0efbc5bac5f44dbd5ab2add1
yes-xodnd <taew3167@gmail.com>
2021-03-03 14:12:13 +0900

    Add Component: PostListLoading

--- src/PostListLoading.js
+++ src/PostListLoading.js
@@ -1,9 +1,25 @@
-import Component from './Component';
+import Component from './Component.js';
 
-class PostListLoading extends Component {
+export default class PostListLoading extends Component {
+
+  template = `
+    <div class="post-loading">
+      <div class="circle"></div>
+    </div>
+  `;
+
   constructor(props) {
     super(props);
     this.init();
-  }
 
+    this.props.subscribe(
+      'isLoading',
+      this.selectElement('.post-loading'),
+      this.toggleLoading
+    );
+  }
+  
+  toggleLoading = e => {
+    e.target.style.display = (this.props.state.isLoading) ? 'block' : 'none';
+  }
 }
